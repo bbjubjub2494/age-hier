1ec446b650257a14c29d6f23e53f1c9d25174b9d
Julie B. <no-reply@bbjubjub.fr>
2023-02-16 18:49:58 +0100

    init

---
+++ cmd/age-hier-derive/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/bbjubjub2494/age-hier-go/impl"
+)
+
+func main() {
+	var i uint
+	flag.UintVar(&i, "index", 0, "index of private key to derive")
+	flag.UintVar(&i, "i", 0, "index of private key to derive")
+	flag.Parse()
+
+	if flag.NArg() > 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	if i >= 1<<31 {
+		log.Fatalln("index out of range (maximum 2**31)")
+	}
+
+	var path = []uint32{44, 753, 0, 0, uint32(i)}
+
+	stdinbuf := bufio.NewReader(os.Stdin)
+	input := func(prompt string) string {
+		os.Stderr.WriteString(prompt)
+		line, err := stdinbuf.ReadString('\n')
+		if err != nil {
+			log.Fatal("error reading", err)
+		}
+		return strings.TrimSpace(line)
+	}
+
+	mnemonic := input("enter mnemonic: ")
+	passphrase := input("enter passphrase: ")
+
+	n, err := impl.FromMnemonic(mnemonic, passphrase)
+	if err != nil {
+		log.Fatalln("invalid mnemonic:", err)
+	}
+
+	for _, d := range path {
+		n = n.Derive(d)
+	}
+
+	enc_sk := n.EncodedPrivateKey()
+	fmt.Println(enc_sk)
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/bbjubjub2494/age-hier-go
+
+go 1.19
+
+require (
+	eagain.net/go/bech32 v0.0.1
+	github.com/anytypeio/go-slip10 v0.0.0-20200330114100-25f30c832993
+	github.com/tyler-smith/go-bip39 v1.1.0
+)
+
+require golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
