ca91eb5babf3ecb93837eb8f8ff3d79a190537ad
Julie B. <no-reply@bbjubjub.fr>
2023-04-06 00:13:46 +0200

    flake: fmt

--- flake.nix
+++ flake.nix
@@ -14,7 +14,6 @@
   nixConfig.extra-substituters = "https://age-hier.cachix.org";
   nixConfig.extra-trusted-public-keys = "age-hier.cachix.org-1:8l0mOCbUxA1HGRXpYfphkNnmchO77eD4UQjef+wfPsM=";
 
-
   outputs = inputs @ {
     flake-parts,
     self,
@@ -42,11 +41,11 @@
         system,
         ...
       }:
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
-
-        let implementations.age-hier-go = pkgs.buildGoModule {
+      # Per-system attributes can be defined here. The self' and inputs'
+      # module parameters provide easy access to attributes of the same
+      # system.
+      let
+        implementations.age-hier-go = pkgs.buildGoModule {
           pname = "age-hier-go";
           version = "unstable";
           src = inputs.go;
@@ -60,16 +59,18 @@
           src = inputs.rust;
           cargoHash = "sha256-6ZfcKqcc8rfyuh+1VTjvOwGsRo17KLMbkirkImngs5A=";
         };
-         in {
-           packages = implementations;
+      in {
+        packages = implementations;
 
-        checks = builtins.mapAttrs (_: impl: pkgs.buildGoModule {
-          name = "age-hier-go-check";
-          src = self;
+        checks = builtins.mapAttrs (_: impl:
+          pkgs.buildGoModule {
+            name = "age-hier-go-check";
+            src = self;
 
-          vendorHash = "sha256-GDw5dPGPv7TGKWiEH2YjtQLKw/jel1V1JWjQAGjfWVw=";
-          nativeCheckInputs = [ impl ];
-        }) implementations;
+            vendorHash = "sha256-GDw5dPGPv7TGKWiEH2YjtQLKw/jel1V1JWjQAGjfWVw=";
+            nativeCheckInputs = [impl];
+          })
+        implementations;
 
         formatter = pkgs.alejandra;
       };
